295f423ae441ab73e5aac583e68f510894c7c718
yoni hachiko <you@example.com>
2025-08-02 12:28:51 -0500

    axios and request

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
 function App() {
   return (
     <div>
-      <h1>hello</h1>
+      <Home />
     </div>
   );
 }
